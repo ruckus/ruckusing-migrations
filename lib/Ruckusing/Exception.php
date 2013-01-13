e7290a3d797e2d0d5444975b144a8b6cbf27d845
Salimane Adjao Moustapha <me@salimane.com>
2013-01-13 20:10:08 +0800

    make Ruckusing_Exception compatible with php 5.2

--- lib/Ruckusing/Exception.php
+++ lib/Ruckusing/Exception.php
@@ -47,7 +47,11 @@ class Ruckusing_Exception extends Exception
     public function __construct($message, $code = 0, Exception $previous = null)
     {
         // make sure everything is assigned properly
-        parent::__construct($message, $code, $previous);
+        if (version_compare(PHP_VERSION, '5.3.0', '>=')) {
+            parent::__construct($message, $code, $previous);
+        } else {
+            parent::__construct($message, $code);
+        }
     }
 
     /**
