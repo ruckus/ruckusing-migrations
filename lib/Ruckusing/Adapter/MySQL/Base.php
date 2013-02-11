93764aa58fa2d11dad388763bd40b55f78ea7a41
Sarah Backhouse <sarahllund@googlemail.com>
2013-02-11 20:18:40 +0000

    Added support for mediumint in MySQL adapter
    Adds support for missing mediumint column type in MySQL adapter.
    
    To use mediumint column type, use 'mediuminteger' as type value when
    creating or updating a column.
    
    Eg. $this->add_column('table', 'column', 'mediuminteger');
    
    Postgresql has no equivalent for mediumint, so falls back to integer
    type when 'mediuminteger' is used.
    
    MySQL 5.0 documentation for the mediumint column type can be found at
    http://dev.mysql.com/doc/refman/5.0/en/integer-types.html

--- lib/Ruckusing/Adapter/MySQL/Base.php
+++ lib/Ruckusing/Adapter/MySQL/Base.php
@@ -108,6 +108,7 @@ class Ruckusing_Adapter_MySQL_Base extends Ruckusing_Adapter_Base implements Ruc
                 'mediumtext'    => array('name' => 'mediumtext'),
                 'integer'       => array('name' => "int", 'limit' => 11),
                 'smallinteger'  => array('name' => "smallint"),
+                'mediuminteger'	=> array('name' => "mediumint"),
                 'biginteger'    => array('name' => "bigint"),
                 'float'         => array('name' => "float"),
                 'decimal'       => array('name' => "decimal", 'scale' => 10, 'precision' => 0),
