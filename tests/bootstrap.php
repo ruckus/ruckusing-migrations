8c5831da3c6ed937ac563b8cb8d10d7145f187cc
Salimane Adjao Moustapha <me@salimane.com>
2013-02-12 13:33:23 +0800

    take care of php 5.2 compatibility

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,13 +2,13 @@
 
 //set up some preliminary defaults, this is so all of our framework includes work
 
-define('RUCKUSING_WORKING_BASE', dirname(__FILE__) . '/dummy/db');
+define('RUCKUSING_WORKING_BASE', dirname(__FILE__) . DIRECTORY_SEPARATOR . 'dummy' . DIRECTORY_SEPARATOR . 'db');
 
-$ruckusing_config = require dirname(__FILE__) . '/../config/database.inc.php';
+$ruckusing_config = require dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'database.inc.php';
 if (isset($ruckusing_config['ruckusing_base'])) {
     define('RUCKUSING_BASE', $ruckusing_config['ruckusing_base']);
 } else {
-    define('RUCKUSING_BASE', dirname(__FILE__) . '/..');
+    define('RUCKUSING_BASE', dirname(__FILE__) . DIRECTORY_SEPARATOR . '..');
 }
 
 define('RUCKUSING_TS_SCHEMA_TBL_NAME', 'schema_migrations');
@@ -18,16 +18,16 @@ if (!defined('BASE')) {
 }
 
 if (!defined('RUCKUSING_TEST_HOME')) {
-    define('RUCKUSING_TEST_HOME', RUCKUSING_BASE . '/tests');
+    define('RUCKUSING_TEST_HOME', RUCKUSING_BASE . DIRECTORY_SEPARATOR . 'tests');
 }
 
-spl_autoload_register('loader', true, true);
+spl_autoload_register('loader', true);
 
 set_include_path(
         implode(
                 PATH_SEPARATOR,
                 array(
-                        RUCKUSING_BASE . '/lib',
+                        RUCKUSING_BASE . DIRECTORY_SEPARATOR . 'lib',
                         get_include_path(),
                 )
         )
@@ -35,8 +35,8 @@ set_include_path(
 
 function loader($classname)
 {
-    $filename = str_replace('_', '/', $classname) . '.php';
-    if (is_file(RUCKUSING_BASE . '/lib/' . $filename)) {
-        include RUCKUSING_BASE . '/lib/' . $filename;
+    $filename = RUCKUSING_BASE . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . str_replace('_', DIRECTORY_SEPARATOR, $classname) . '.php';
+    if (is_file($filename)) {
+        include $filename;
     }
 }
