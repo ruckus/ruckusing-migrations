740c8688a6a74367c7e10b06a24e37947997c190
victorbjelkholm <victorbjelkholm@gmail.com>
2013-06-28 18:16:41 +0200

    Rename db:schema to db:structure and replaced db:schema to also dump the migration versions
    To be able to really use migrations in a way that makes sense, you need to know in what version the
    database was when you made the schema dump so anyone can run a copy of the output and be up and
    running. Otherwise, the database doesn't know in what version it is and will always try to run
    all the migrations again which isn't working well.
    
    For those that still only want the structure, there is now a db:structure command instead.

--- lib/Task/Db/Structure.php
+++ lib/Task/Db/Structure.php
@@ -21,7 +21,7 @@
  * @author   Cody Caughlan <codycaughlan % gmail . com>
  * @link      https://github.com/ruckus/ruckusing-migrations
  */
-class Task_Db_Schema extends Ruckusing_Task_Base implements Ruckusing_Task_Interface
+class Task_Db_Structure extends Ruckusing_Task_Base implements Ruckusing_Task_Interface
 {
     /**
      * Current Adapter
@@ -38,11 +38,11 @@ class Task_Db_Schema extends Ruckusing_Task_Base implements Ruckusing_Task_Inter
     private $_return = '';
 
     /**
-     * Creates an instance of Task_DB_Schema
+     * Creates an instance of Task_DB_Structure
      *
      * @param Ruckusing_Adapter_Base $adapter The current adapter being used
      *
-     * @return Task_DB_Schema
+     * @return Task_DB_Structure
      */
     public function __construct($adapter)
     {
@@ -58,12 +58,12 @@ class Task_Db_Schema extends Ruckusing_Task_Base implements Ruckusing_Task_Inter
     public function execute($args)
     {
         $this->_return .= "Started: " . date('Y-m-d g:ia T') . "\n\n";
-        $this->_return .= "[db:schema]: \n";
+        $this->_return .= "[db:structure]: \n";
 
         //write to disk
-        $schema_file = $this->db_dir() . '/schema.txt';
-        $schema = $this->_adapter->schema($schema_file);
-        $this->_return .= "\tSchema written to: $schema_file\n\n";
+        $structure_file = $this->db_dir() . '/structure.txt';
+        $structure = $this->_adapter->structure($structure_file);
+        $this->_return .= "\tStructure written to: $structure_file\n\n";
         $this->_return .= "\n\nFinished: " . date('Y-m-d g:ia T') . "\n\n";
 
         return $this->_return;
@@ -79,7 +79,7 @@ class Task_Db_Schema extends Ruckusing_Task_Base implements Ruckusing_Task_Inter
         // create the db directory if it doesnt exist
         $db_directory = $this->get_framework()->db_directory();
         if (!is_dir($db_directory)) {
-            $this->_return .= sprintf("\n\tDB Schema directory (%s doesn't exist, attempting to create.\n", $db_directory);
+            $this->_return .= sprintf("\n\tDB Structure directory (%s doesn't exist, attempting to create.\n", $db_directory);
             if (mkdir($db_directory, 0755, true) === FALSE) {
                 $this->_return .= sprintf("\n\tUnable to create migrations directory at %s, check permissions?\n", $db_directory);
             } else {
@@ -90,7 +90,7 @@ class Task_Db_Schema extends Ruckusing_Task_Base implements Ruckusing_Task_Inter
         //check to make sure our destination directory is writable
         if (!is_writable($db_directory)) {
             throw new Ruckusing_Exception(
-                    "ERROR: DB Schema directory '"
+                    "ERROR: DB Structure directory '"
                     . $db_directory
                     . "' is not writable by the current user. Check permissions and try again.\n",
                     Ruckusing_Exception::INVALID_DB_DIR
@@ -109,12 +109,12 @@ class Task_Db_Schema extends Ruckusing_Task_Base implements Ruckusing_Task_Inter
     {
         $output =<<<USAGE
 
-\tTask: db:schema
+\tTask: db:structure
 
 \tIt can be beneficial to get a dump of the DB in raw SQL format which represents
 \tthe current version.
 
-\tNote: This dump only contains the actual schema (e.g. the DML needed to
+\tNote: This dump only contains the actual structure (e.g. the DML needed to
 \treconstruct the DB), but not any actual data.
 
 \tIn MySQL terms, this task would not be the same as running the mysqldump command
