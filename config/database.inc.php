767bded97b959626aaf4d84ebe3ec0a0e5072dfe
piotrooo <piotroo89@gmail.com>
2014-02-27 13:51:34 +0100

    cleanup.

--- config/database.inc.php
+++ config/database.inc.php
@@ -8,10 +8,9 @@
 
 Valid types (adapters) are Postgres & MySQL:
 
-'type' must be one of: 'pgsql' or 'mysql'
+'type' must be one of: 'pgsql' or 'mysql' or 'sqlite'
 
 */
-
 return array(
     'db' => array(
         'development' => array(
@@ -20,34 +19,31 @@ return array(
             'port' => 3306,
             'database' => 'ruckusing_migrations',
             'user' => 'root',
-            'password' => 'root',
+            'password' => 'piotr123',
             //'charset' => 'utf8',
             //'directory' => 'custom_name',
             //'socket' => '/var/run/mysqld/mysqld.sock'
         ),
-
         'pg_test' => array(
             'type' => 'pgsql',
             'host' => 'localhost',
             'port' => 5432,
             'database' => 'ruckusing_migrations_test',
             'user' => 'postgres',
-            'password' => '',
+            'password' => 'piotr123',
             //'directory' => 'custom_name',
 
         ),
-
         'mysql_test' => array(
             'type' => 'mysql',
             'host' => 'localhost',
             'port' => 3306,
             'database' => 'ruckusing_migrations_test',
             'user' => 'root',
-            'password' => 'root',
+            'password' => 'piotr123',
             //'directory' => 'custom_name',
             //'socket' => '/var/run/mysqld/mysqld.sock'
         ),
-
         'sqlite_test' => array(
             'type' => 'sqlite',
             'database' => '/tmp/ruckusing_migrations_test.sqlite3',
@@ -58,13 +54,8 @@ return array(
         )
 
     ),
-
     'migrations_dir' => array('default' => RUCKUSING_WORKING_BASE . '/migrations'),
-
     'db_dir' => RUCKUSING_WORKING_BASE . DIRECTORY_SEPARATOR . 'db',
-
     'log_dir' => RUCKUSING_WORKING_BASE . DIRECTORY_SEPARATOR . 'logs',
-
     'ruckusing_base' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..'
-
 );
